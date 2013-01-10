4ae2a0a1350d5a0694930a1c2d9b2e04c997fd7f
feeloc <hjm1989@gmail.com>
2013-01-10 17:17:28 +0800

    init project

---
+++ config.js
@@ -0,0 +1,16 @@
+/*!
+ * todo - config.js
+ * Copyright(c) 2012 fengmk2 <fengmk2@gmail.com>
+ * MIT Licensed
+ */
+
+"use strict";
+
+exports.debug = true;
+exports.port = 3000;
+exports.email = 'hjm1989@gmail.com';
+exports.site_name = '果果的工作日志';
+exports.site_desc = '每天记一点，成长多一点';
+exports.session_secret = 'todo session secret';
+
+exports.db = 'mongodb://127.0.0.1/mongodb';
